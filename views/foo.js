e84175e24ffc19807d4266369269b70cb1537dad
Will Ware <wware@alum.mit.edu>
2014-11-30 10:46:10 -0500

    Get rid of ciphersaber, use only Rijndael.

--- views/foo.js
+++ views/foo.js
@@ -17,7 +17,6 @@ $(function() {
     var blankButton = $('.blank-button');
     var setupButton = $('.setup-button');
     var showPasswordButton = $('.showpw-button');
-    var cipher = ciphersaber;
     var password = null;
 
     try {
@@ -31,25 +30,18 @@ $(function() {
         return;
     }
 
-    var cipher;
-    // Maybe make it a user option to choose which cipher?
-    if (false) {
-        cipher = ciphersaber;
-    } else {
-        // use rijndael instead
-        cipher = {
-            encrypt: function(key, plaintext) {
-                var iv = cipherutils.makePassword(16);
-                var encrypted = mcrypt.Encrypt(plaintext, iv, key, 'rijndael-128', 'cfb');
-                return iv + encrypted;
-            },
-            decrypt: function(key, s) {
-                var iv = s.substring(0, 16);
-                var ciphertext = s.substring(16);
-                return mcrypt.Decrypt(ciphertext, iv, key, 'rijndael-128', 'cfb');
-            }
-        };
-    }
+    var cipher = {
+        encrypt: function(key, plaintext) {
+            var iv = cipherutils.makePassword(16);
+            var encrypted = mcrypt.Encrypt(plaintext, iv, key, 'rijndael-128', 'cfb');
+            return iv + encrypted;
+        },
+        decrypt: function(key, s) {
+            var iv = s.substring(0, 16);
+            var ciphertext = s.substring(16);
+            return mcrypt.Decrypt(ciphertext, iv, key, 'rijndael-128', 'cfb');
+        }
+    };
 
     var goToLogin = function() {
         blank();
@@ -147,13 +139,11 @@ $(function() {
         });
     };
 
-    onGetPin(pin, function(event) {
-        attemptDecryption(ciphersaber, event);
-    });
+    onGetPin(pin, attemptDecryption);
 
     onGetPin(pinForPassword, function(event) {
         passwordShow.text('');
-        if (getPassword(ciphersaber, pinForPassword, event)) {
+        if (getPassword(cipher, pinForPassword, event)) {
             passwordShow.text(password);
         }
     });
