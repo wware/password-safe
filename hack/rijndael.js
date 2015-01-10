3098cfd03fd6b88e4b933272b243a5e257552aca
Will Ware <wware@alum.mit.edu>
2015-01-10 09:18:38 -0500

    Use a directive to filter data-binding in both directions.

--- hack/rijndael.js
+++ hack/rijndael.js
@@ -16,105 +16,104 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  *  02111-1307 USA
  */
-var Rijndael = Rijndael ? Rijndael : new function() {
-    /*
-     *
-     * This is a javascript implementation of the rijndael block cipher. Key lengths
-     * of 128, 192 and 256 bits, and block lengths of 128, 192, and 256 bit in any
-     * combination are supported.
-     *
-     * The well-functioning of the encryption/decryption routines has been
-     * verified for different key lengths with the test vectors given in
-     * FIPS-197, Appendix C.
-     *
-     * The following code example enciphers the plaintext block '00 11 22 .. EE FF'
-     * with the 256 bit key '12345678911234567892123456789312'.
-     *
-     *    var block = new Array(16);
-     *    for(var i = 0; i < 16; i++)
-     *        block[i] = 0x11 * i;
-     *
-     *    var key = '12345678911234567892123456789312';
-     *
-     *    rijndael.Encrypt(block, key);
-     *
-     */
 
-    /******************************************************************************/
+/*
+ * Adapted from https://code.google.com/p/js-mcrypt/source/browse/trunk/rijndael.js
+ * Cipher-block chaining added, see
+ * https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher-block_chaining_.28CBC.29
+ *
+ * This is a javascript implementation of the rijndael block cipher. Key lengths
+ * of 128, 192 and 256 bits, and block lengths of 128, 192, and 256 bit in any
+ * combination are supported.
+ *
+ * The well-functioning of the encryption/decryption routines has been
+ * verified for different key lengths with the test vectors given in
+ * FIPS-197, Appendix C.
+ */
 
-    //public
-    var pub = {};
-
-    var BlockEncrypt = function(block, key) {
+var rijndael = function () {
+    var publicInterface = {};
+    var BlockEncrypt = function (block, key) {
         block = CopyBlock(block);
         crypt(block, key, true);
         return block;
     };
-    var BlockDecrypt = function(block, key) {
+    var BlockDecrypt = function (block, key) {
         block = CopyBlock(block);
         crypt(block, key, false);
         return block;
     };
-    var CopyBlock = function(block) {
+    var CopyBlock = function (block) {
         var newblock = new Array(16);
-        for (i = 0; i < 16; i++)
+        for (i = 0; i < 16; i++) {
             newblock[i] = block[i];
+        }
         return newblock;
     };
-    var ZeroBlock = function() {
+    var ZeroBlock = function () {
         var block = new Array(16);
-        for (i = 0; i < 16; i++)
+        for (i = 0; i < 16; i++) {
             block[i] = 0;
+        }
         return block;
     };
-    var XorBlock = function(blk1, blk2) {
+    var XorBlock = function (blk1, blk2) {
         var block = new Array(16);
-        for (i = 0; i < 16; i++)
+        for (i = 0; i < 16; i++) {
             block[i] = blk1[i] ^ blk2[i];
+        }
         return block;
     };
-    var StrBlock = function(str, n) {
+    var StrBlock = function (str, n) {
         var block = new Array(16);
-        for (i = 0; i < 16; i++)
+        for (i = 0; i < 16; i++) {
             block[i] = str.charCodeAt(n + i);
+        }
         return block;
     };
 
-    pub.Encrypt = function(str, key) {
+    publicInterface.Encrypt = function (str, key) {
         var i, blk, previousBlock = null;
         var n = str.length;
-        var numblocks = Math.floor((n - 2) / 16) + 2;
-        for (i = n; i < 16 * numblocks; i++)
+        var numblocks = Math.floor((n - 1) / 16) + 2;
+        for (i = n; i < 16 * numblocks; i++) {
             str += "\0";
+        }
         var lst = [n];
         for (i = 0; i < numblocks; i++) {
             blk = StrBlock(str, i * 16);
-            if (previousBlock !== null)
+            if (previousBlock !== null) {
                 blk = XorBlock(blk, previousBlock);
+            }
             blk = BlockEncrypt(blk, key);
             lst.push(blk);
             previousBlock = blk;
         }
         return lst;
-    }
-    pub.Decrypt = function(lst, key) {
+    };
+    publicInterface.Decrypt = function (lst, key) {
         var n = lst[0];
-        var i, j, str = "", blk, nblk, previousBlock = null;
+        var i, j, str = "",
+            blk, nblk, previousBlock = null;
         var numblocks = lst.length - 1;
         for (i = 1; i <= numblocks; i++) {
             blk = nblk = lst[i];
             blk = BlockDecrypt(blk, key);
-            if (previousBlock !== null)
+            if (previousBlock !== null) {
                 blk = XorBlock(blk, previousBlock);
+            }
             previousBlock = nblk;
-            for (j = 0; j < 16; j++)
+            for (j = 0; j < 16; j++) {
                 str += String.fromCharCode(blk[j]);
+            }
         }
-        for (i = 0; j && i < 16; i++)
-            if (str[str.length - i - 1] !== "\0")
-                return null;
+        for (i = n; i < str.length; i++) {
+            if (str[i] !== "\0") {
+                return "Decryption failure";
+            }
+        }
         return str.substr(0, n);
-    }
+    };
 
     //private
 
@@ -129,44 +128,43 @@ var Rijndael = Rijndael ? Rijndael : new function() {
 
     var expandedKeys = {}; //object to keep keys we've already expanded in.
 
-    var ExpandKey = function(key) {
+    var ExpandKey = function (key) {
         if (!expandedKeys[key]) {
             var kl = key.length,
                 ks, Rcon = 1;
             ks = 15 << 5;
             keyA = new Array(ks);
             for (var i = 0; i < kl; i++)
-                keyA[i] = key.charCodeAt(i);
+            keyA[i] = key.charCodeAt(i);
             for (i = kl; i < ks; i += 4) {
                 var temp = keyA.slice(i - 4, i);
                 if (i % kl === 0) {
                     temp = [Sbox[temp[1]] ^ Rcon, Sbox[temp[2]],
-                        Sbox[temp[3]], Sbox[temp[0]]
-                    ];
-                    if ((Rcon <<= 1) >= 256)
-                        Rcon ^= 0x11b;
-                } else if ((kl > 24) && (i % kl == 16))
-                    temp = [Sbox[temp[0]], Sbox[temp[1]],
-                        Sbox[temp[2]], Sbox[temp[3]]
-                    ];
+                    Sbox[temp[3]], Sbox[temp[0]]];
+                    if ((Rcon <<= 1) >= 256) Rcon ^= 0x11b;
+                } else if ((kl > 24) && (i % kl == 16)) temp = [Sbox[temp[0]], Sbox[temp[1]],
+                Sbox[temp[2]], Sbox[temp[3]]];
                 for (var j = 0; j < 4; j++)
-                    keyA[i + j] = keyA[i + j - kl] ^ temp[j];
+                keyA[i + j] = keyA[i + j - kl] ^ temp[j];
             }
             expandedKeys[key] = keyA;
         }
         return expandedKeys[key];
-    }
+    };
 
-    var crypt = function(block, key, encrypt) {
+    var crypt = function (block, key, encrypt) {
+        var i, SRT;
         var bB = block.length;
         var kB = key.length;
         var bBi = 0;
         var kBi = 0;
         switch (bB) {
             case 32:
-                bBi++;
+                bBi += 2;
+                break;
             case 24:
                 bBi++;
+                break;
             case 16:
                 break;
             default:
@@ -174,9 +172,11 @@ var Rijndael = Rijndael ? Rijndael : new function() {
         }
         switch (kB) {
             case 32:
-                kBi++;
+                kBi += 2;
+                break;
             case 24:
                 kBi++;
+                break;
             case 16:
                 break;
             default:
@@ -187,8 +187,8 @@ var Rijndael = Rijndael ? Rijndael : new function() {
         var end = r * bB;
         if (encrypt) {
             AddRoundKey(block, key.slice(0, bB));
-            var SRT = ShiftRowTab[bBi];
-            for (var i = bB; i < end; i += bB) {
+            SRT = ShiftRowTab[bBi];
+            for (i = bB; i < end; i += bB) {
                 SubBytes(block, Sbox);
                 ShiftRows(block, SRT);
                 MixColumns(block);
@@ -210,23 +210,22 @@ var Rijndael = Rijndael ? Rijndael : new function() {
             }
             AddRoundKey(block, key.slice(0, bB));
         }
-    }
-    /******************************************************************************/
+    };
 
     /* The following lookup tables and functions are for internal use only! */
     var Sbox = new Array(99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171,
-        118, 202, 130, 201, 125, 250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192, 183, 253,
-        147, 38, 54, 63, 247, 204, 52, 165, 229, 241, 113, 216, 49, 21, 4, 199, 35, 195, 24, 150, 5, 154,
-        7, 18, 128, 226, 235, 39, 178, 117, 9, 131, 44, 26, 27, 110, 90, 160, 82, 59, 214, 179, 41, 227,
-        47, 132, 83, 209, 0, 237, 32, 252, 177, 91, 106, 203, 190, 57, 74, 76, 88, 207, 208, 239, 170,
-        251, 67, 77, 51, 133, 69, 249, 2, 127, 80, 60, 159, 168, 81, 163, 64, 143, 146, 157, 56, 245,
-        188, 182, 218, 33, 16, 255, 243, 210, 205, 12, 19, 236, 95, 151, 68, 23, 196, 167, 126, 61,
-        100, 93, 25, 115, 96, 129, 79, 220, 34, 42, 144, 136, 70, 238, 184, 20, 222, 94, 11, 219, 224,
-        50, 58, 10, 73, 6, 36, 92, 194, 211, 172, 98, 145, 149, 228, 121, 231, 200, 55, 109, 141, 213,
-        78, 169, 108, 86, 244, 234, 101, 122, 174, 8, 186, 120, 37, 46, 28, 166, 180, 198, 232, 221,
-        116, 31, 75, 189, 139, 138, 112, 62, 181, 102, 72, 3, 246, 14, 97, 53, 87, 185, 134, 193, 29,
-        158, 225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223, 140, 161,
-        137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22);
+    118, 202, 130, 201, 125, 250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192, 183, 253,
+    147, 38, 54, 63, 247, 204, 52, 165, 229, 241, 113, 216, 49, 21, 4, 199, 35, 195, 24, 150, 5, 154,
+    7, 18, 128, 226, 235, 39, 178, 117, 9, 131, 44, 26, 27, 110, 90, 160, 82, 59, 214, 179, 41, 227,
+    47, 132, 83, 209, 0, 237, 32, 252, 177, 91, 106, 203, 190, 57, 74, 76, 88, 207, 208, 239, 170,
+    251, 67, 77, 51, 133, 69, 249, 2, 127, 80, 60, 159, 168, 81, 163, 64, 143, 146, 157, 56, 245,
+    188, 182, 218, 33, 16, 255, 243, 210, 205, 12, 19, 236, 95, 151, 68, 23, 196, 167, 126, 61,
+    100, 93, 25, 115, 96, 129, 79, 220, 34, 42, 144, 136, 70, 238, 184, 20, 222, 94, 11, 219, 224,
+    50, 58, 10, 73, 6, 36, 92, 194, 211, 172, 98, 145, 149, 228, 121, 231, 200, 55, 109, 141, 213,
+    78, 169, 108, 86, 244, 234, 101, 122, 174, 8, 186, 120, 37, 46, 28, 166, 180, 198, 232, 221,
+    116, 31, 75, 189, 139, 138, 112, 62, 181, 102, 72, 3, 246, 14, 97, 53, 87, 185, 134, 193, 29,
+    158, 225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223, 140, 161,
+    137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22);
     //row	0	1	2	3		block Bytes
     var rowshifts = [
         [0, 1, 2, 3], //16
@@ -234,20 +233,21 @@ var Rijndael = Rijndael ? Rijndael : new function() {
         [0, 1, 3, 4]
     ]; //32
 
+    var i, j;
     var ShiftRowTab = Array(3);
-    for (var i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++) {
         ShiftRowTab[i] = Array(sizes[i]);
-        for (var j = sizes[i]; j >= 0; j--)
-            ShiftRowTab[i][j] = (j + (rowshifts[i][j & 3] << 2)) % sizes[i];
+        for (j = sizes[i]; j >= 0; j--)
+        ShiftRowTab[i][j] = (j + (rowshifts[i][j & 3] << 2)) % sizes[i];
     }
     var Sbox_Inv = new Array(256);
     for (i = 0; i < 256; i++)
-        Sbox_Inv[Sbox[i]] = i;
+    Sbox_Inv[Sbox[i]] = i;
     var ShiftRowTab_Inv = Array(3);
     for (i = 0; i < 3; i++) {
         ShiftRowTab_Inv[i] = Array(sizes[i]);
         for (j = sizes[i]; j >= 0; j--)
-            ShiftRowTab_Inv[i][ShiftRowTab[i][j]] = j;
+        ShiftRowTab_Inv[i][ShiftRowTab[i][j]] = j;
     }
     var xtime = new Array(256);
     for (i = 0; i < 128; i++) {
@@ -255,23 +255,23 @@ var Rijndael = Rijndael ? Rijndael : new function() {
         xtime[128 + i] = (i << 1) ^ 0x1b;
     }
 
-    var SubBytes = function(state, sbox) {
+    var SubBytes = function (state, sbox) {
         for (var i = state.length - 1; i >= 0; i--)
-            state[i] = sbox[state[i]];
-    }
+        state[i] = sbox[state[i]];
+    };
 
-    var AddRoundKey = function(state, rkey) {
+    var AddRoundKey = function (state, rkey) {
         for (var i = state.length - 1; i >= 0; i--)
-            state[i] ^= rkey[i];
-    }
+        state[i] ^= rkey[i];
+    };
 
-    var ShiftRows = function(state, shifttab) {
+    var ShiftRows = function (state, shifttab) {
         var h = state.slice(0);
         for (var i = state.length - 1; i >= 0; i--)
-            state[i] = h[shifttab[i]];
-    }
+        state[i] = h[shifttab[i]];
+    };
 
-    var MixColumns = function(state) {
+    var MixColumns = function (state) {
         for (var i = state.length - 4; i >= 0; i -= 4) {
             var s0 = state[i + 0],
                 s1 = state[i + 1];
@@ -283,9 +283,9 @@ var Rijndael = Rijndael ? Rijndael : new function() {
             state[i + 2] ^= h ^ xtime[s2 ^ s3];
             state[i + 3] ^= h ^ xtime[s3 ^ s0];
         }
-    }
+    };
 
-    var MixColumns_Inv = function(state) {
+    var MixColumns_Inv = function (state) {
         for (var i = state.length - 4; i >= 0; i -= 4) {
             var s0 = state[i + 0],
                 s1 = state[i + 1];
@@ -300,13 +300,7 @@ var Rijndael = Rijndael ? Rijndael : new function() {
             state[i + 2] ^= h1 ^ xtime[s2 ^ s3];
             state[i + 3] ^= h2 ^ xtime[s3 ^ s0];
         }
-    }
-    return pub;
-};
+    };
 
-if (typeof module !== 'undefined') {
-    // node.js require
-    for (var key in Rijndael) {
-        module.exports[key] = Rijndael[key];
-    }
-}
+    return publicInterface;
+};
--- hack/script.js
+++ hack/script.js
@@ -1,168 +1,82 @@
-var CryptoSynchronizer = function(
-    plainSetter, plainGetter, cipherSetter, cipherGetter,
-    keyGetter, threeStrikes) {
+var R = rijndael();
 
-    var numFailedDecryptions = 0;
-    var pub = {};
-
-    pub.encrypt = function() {
-        var x = plainGetter();
-        var key = keyGetter();
-        var y = Rijndael.Encrypt(x, key);
-        numFailedDecryptions = 0;
-        cipherSetter(y);
-    };
-
-    pub.decrypt = function() {
-        var x = cipherGetter();
-        var key = keyGetter();
-        var y = Rijndael.Decrypt(x, key);
-        if (y === null) {
-            numFailedDecryptions++;
-            if (threeStrikes && numFailedDecryptions == 3) {
-                threeStrikes();
-                numFailedDecryptions = 0;
-                return;
+var directiveBuilder = function (keyname) {
+    var nullValue = [0,
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    var _keyname = keyname;
+    return function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModel) {
+                /*
+                 * Force fresh decryptions when keys change.
+                 */
+                // TODO instead of referencing scope.data.var1,
+                // use the dotted contents of attr.ngModel ("data.var1")
+                // so that this will also work with var2.
+                scope.$watch(function() {
+                    return scope[_keyname];
+                }, function() {
+                    var value = scope.data.var1;
+                    setTimeout(function () {
+                        scope.data.var1 = nullValue;
+                        scope.$apply();
+                        setTimeout(function () {
+                            scope.data.var1 = value;
+                            scope.$apply();
+                        }, 0);
+                    }, 0);
+                });
+                function fromUI(text) {
+                    return R.Encrypt(text || '', scope[_keyname]);
+                }
+    
+                function toUI(text) {
+                    return R.Decrypt(text || '', scope[_keyname]);
+                }
+                ngModel.$parsers.push(fromUI);
+                ngModel.$formatters.push(toUI);
             }
-        }
-        plainSetter(y);
+        };
     };
-
-    return pub;
 };
 
-var myApp = angular.module('myApp', []);
+var module = angular.module("demo", []);
 
-myApp.controller('PasswordSafeController', ['$scope',
-    function($scope) {
-        var pinLockedKey;
-        // $scope.key = '12345678911234567892123456789312';
-        $scope.key = '123456789\1011234567892123456789312';
-        $scope.pin = '1234';
-        var csyncKey = CryptoSynchronizer(
-            function(x) {
-                $scope.key = x;
-            },
-            function() {
-                return $scope.key;
-            },
-            function(x) {
-                pinLockedKey = x;
-            },
-            function() {
-                return pinLockedKey;
-            },
-            function() {
-                var pin = $scope.pin;
-                while (pin.length < 16)
-                    pin += "\0";
-                return pin;
-            },
-            function() {
-                /* nothing for now */
-            });
-        csyncKey.encrypt();
-        $scope.key = '';
-        $scope.unlockKey = function() {
-            csyncKey.decrypt();
+module.directive('cryptosync', directiveBuilder('key1'));
+module.directive('cryptosync2', directiveBuilder('key2'));
+
+module.controller("Controller1", ["$scope", function($scope) {
+    $scope.key1 = "0123456789ABCDEF";
+    $scope.key2 = "89ABCDEF01234567";
+
+    $scope.data = {
+        var1: [0,
+            [157, 44, 218, 144, 27, 104, 45, 51, 89, 112, 154, 90, 178, 65, 150, 36]],
+        var2:[0,
+            [172, 236, 236, 171, 165, 179, 206, 197, 26, 137, 183, 150, 236, 32, 159, 93]]
+    };
+
+    $scope.loadData = function () {
+        $scope.data = {
+            var1: [3, [238, 179, 26, 7, 162, 104, 145, 98, 136, 182, 53, 197, 132, 4, 32, 167],
+                [172, 143, 61, 224, 243, 166, 11, 12, 154, 131, 147, 124, 76, 108, 71, 200]
+            ],
+            var2: [3, [19, 184, 32, 17, 93, 71, 45, 157, 89, 41, 30, 26, 123, 124, 8, 182],
+                [107, 77, 0, 17, 8, 49, 80, 51, 255, 116, 11, 169, 154, 77, 136, 248]
+            ]
         };
-        $scope.clearKey = function() {
-            $scope.key = "";
+    };
+
+    $scope.loadBadData = function () {
+        $scope.data = {
+            var1: [3, [238, 179, 26, 7, 162, 104, 145, 98, 136, 182, 53, 197, 132, 4, 32, 167],
+                [172, 143, 61, 224, 243, 166, 11, 12, 154, 131, 147, 124, 76, 108, 71, 199]
+            ],
+            var2: [3, [19, 184, 32, 17, 93, 71, 45, 157, 89, 41, 30, 26, 123, 124, 8, 182],
+                [107, 77, 0, 17, 8, 49, 80, 51, 255, 116, 11, 169, 154, 77, 136, 247]
+            ]
         };
-        $scope.saveKey = function() {
-            csyncKey.encrypt();
-            $scope.key = "";
-        };
-        var keyLockedContent = [466,
-            [3, 19, 102, 223, 109, 152, 107, 188, 40, 145, 237, 184, 253, 208, 196, 143],
-            [116, 165, 65, 127, 7, 10, 4, 171, 39, 195, 160, 204, 239, 105, 53, 79],
-            [102, 247, 228, 29, 75, 186, 212, 132, 120, 7, 21, 200, 24, 30, 21, 76],
-            [32, 161, 31, 184, 87, 90, 23, 112, 195, 138, 125, 223, 227, 113, 38, 186],
-            [136, 222, 139, 251, 105, 1, 107, 178, 137, 177, 20, 22, 141, 32, 123, 11],
-            [149, 182, 194, 206, 166, 115, 118, 9, 59, 108, 80, 56, 82, 72, 76, 31],
-            [197, 231, 171, 95, 205, 203, 106, 169, 28, 148, 148, 76, 162, 249, 112, 39],
-            [151, 222, 255, 4, 194, 53, 72, 246, 87, 53, 161, 21, 30, 38, 54, 102],
-            [226, 231, 36, 87, 194, 228, 211, 67, 205, 44, 86, 240, 247, 21, 66, 100],
-            [185, 119, 73, 15, 226, 73, 152, 43, 15, 44, 100, 134, 24, 177, 186, 147],
-            [36, 129, 64, 15, 123, 47, 70, 150, 229, 95, 114, 52, 47, 5, 108, 244],
-            [69, 185, 81, 29, 0, 34, 193, 135, 247, 179, 212, 153, 191, 191, 36, 77],
-            [188, 70, 145, 204, 152, 254, 80, 17, 131, 160, 32, 168, 248, 200, 70, 205],
-            [240, 56, 0, 25, 201, 188, 133, 157, 18, 159, 167, 85, 221, 118, 247, 168],
-            [98, 209, 110, 228, 119, 6, 169, 189, 183, 47, 172, 82, 94, 160, 66, 69],
-            [185, 45, 178, 193, 88, 35, 63, 35, 1, 31, 182, 28, 196, 60, 112, 9],
-            [115, 34, 156, 73, 169, 228, 193, 222, 101, 187, 1, 92, 44, 47, 95, 157],
-            [164, 149, 94, 129, 65, 80, 78, 249, 130, 212, 214, 190, 168, 26, 173, 82],
-            [249, 190, 83, 69, 158, 112, 1, 224, 92, 123, 44, 150, 145, 212, 36, 72],
-            [196, 252, 5, 174, 156, 197, 97, 6, 10, 96, 107, 248, 169, 16, 117, 158],
-            [163, 162, 199, 229, 125, 151, 130, 171, 29, 173, 0, 239, 195, 205, 99, 217],
-            [223, 78, 195, 135, 59, 0, 153, 60, 159, 130, 230, 74, 201, 125, 142, 160],
-            [53, 28, 100, 48, 220, 41, 57, 157, 217, 163, 76, 160, 237, 247, 135, 224],
-            [2, 222, 100, 33, 62, 196, 26, 241, 247, 211, 42, 26, 140, 219, 22, 79],
-            [128, 240, 130, 148, 118, 134, 103, 68, 25, 191, 165, 252, 186, 110, 90, 47],
-            [181, 65, 0, 35, 179, 100, 66, 73, 59, 170, 104, 198, 149, 104, 164, 27],
-            [9, 146, 242, 204, 241, 102, 74, 24, 47, 240, 184, 80, 153, 23, 14, 68],
-            [16, 165, 180, 199, 238, 23, 175, 56, 87, 103, 111, 105, 9, 110, 193, 76],
-            [77, 18, 185, 72, 111, 72, 94, 64, 153, 69, 38, 227, 192, 51, 221, 118],
-            [125, 159, 89, 167, 143, 96, 198, 1, 201, 137, 34, 185, 48, 163, 190, 81],
-            [7, 230, 249, 26, 52, 156, 91, 78, 55, 184, 54, 38, 98, 143, 112, 137]
-        ];
-        $scope.secrets = "";
-        var csyncContent = CryptoSynchronizer(
-            function(x) {
-                $scope.secrets = x;
-            },
-            function() {
-                return $scope.secrets;
-            },
-            function(x) {
-                keyLockedContent = x;
-            },
-            function() {
-                return keyLockedContent;
-            },
-            function() {
-                return $scope.key;
-            },
-            function() {
-                /* nothing for now */
-            });
-        $scope.unlockContent = function() {
-            csyncContent.decrypt();
-        };
-        $scope.clearContent = function() {
-            $scope.secrets = "";
-        };
-        $scope.saveContent = function() {
-            csyncContent.encrypt();
-            $scope.secrets = "";
-        };
-        $scope.secrets = "";
-        var csyncContent = CryptoSynchronizer(
-            function(x) {
-                $scope.secrets = x;
-            },
-            function() {
-                return $scope.secrets;
-            },
-            function(x) {
-                keyLockedContent = x;
-            },
-            function() {
-                return keyLockedContent;
-            },
-            function() {
-                return $scope.key;
-            },
-            function() { /* nothing for now */ });
-        $scope.unlockContent = function() {
-            csyncContent.decrypt();
-        };
-        $scope.clearContent = function() {
-            $scope.secrets = "";
-        };
-        $scope.saveContent = function() {
-            csyncContent.encrypt();
-            $scope.secrets = "";
-        };
-    }
-]);
+    };
+}]);
